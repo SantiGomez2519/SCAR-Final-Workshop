f6df4b8327e16cd97b9864014af05e9b764c5085
SantiGomez2519 <sgomezo14@eafit.edu.co>
2024-07-29 22:07:55 -0500

    Change in the matrix traversal and added comments

--- mmm_implementation.c
+++ mmm_implementation.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <omp.h>
-#include <immintrin.h>
 
+//OpenMP to parallelize and vectorize the loops
+#include <omp.h>
+
+//To compile gcc mmm_implementation.c -o bin/mmm -O3 -ftree-vectorize -fopenmp -mavx2 -fopt-info-vec-optimized
 
 //Original N 50 and MAX_SIZE 5000
-#define N 10
+#define N 50
 #define MIN_SIZE 2
-#define MAX_SIZE 1000
+#define MAX_SIZE 5000
 #define MAX_NUM 10000000.0
 
 int main(){
-    // Estimado Wambi, la única modificación que voy a hacer acá es agregar el set de los hilos <3 
-    omp_set_num_threads(4);
 
     srand((unsigned) time(NULL));
-    // int size = MIN_SIZE + (rand() % MAX_SIZE), iterator = 0;
-    int size = 500, iterator = 0;
+    int size = MIN_SIZE + (rand() % MAX_SIZE), iterator = 0;
     double time_spent = 0.0;
     double A[size][size] __attribute__((aligned(64))), B[size][size] __attribute__((aligned(64))), C[size][size] __attribute__((aligned(64)));
 
@@ -27,31 +26,27 @@ int main(){
         clock_t begin_time = clock();
         
         // Start modifying here
-
         
+        //Parallelize the loops with OpenMP
         #pragma omp critical
         {
-        #pragma omp simd
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
-                A[j][i] = rand();
-                B[j][i] = rand(); 
-                C[j][i] = 0;
-            }
-        }
-    
-        }
-
-        #pragma omp critical
-        {
-        #pragma omp simd
-        for(int j = 0; j < size; j++){
-            for(int k = 0; k < size; k++){
-                for(int i = 0; i < size; i++){
-                    C[i][j] += A[i][k] * B[k][j];
+            //Vectorize the loops with Single Instruction Multiple Data (SIMD)
+            #pragma omp simd
+            for(int i = 0; i < size; i++){
+                for(int j = 0; j < size; j++){
+                    A[i][j] = rand();
+                    B[i][j] = rand(); 
+                    C[i][j] = 0;
                 }
             }
-        }
+        
+            for(int j = 0; j < size; j++){
+                for(int k = 0; k < size; k++){
+                    for(int i = 0; i < size; i++){
+                        C[j][i] += A[j][k] * B[k][i];
+                    }
+                }
+            }
         }
 
 
